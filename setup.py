f2b3df1f38382ecc4988957c42cb8139b942002f
George Tantiras <raratiru@users.noreply.github.com>
2019-12-05 02:16:08 +0200

    Testing Django-3.0 (#19)
    * Update Tests
    
    * Update Tox
    
    * Update Readme
    
    * Update setup.py
    
    * Update requirements.txt
    
    * Update .travis.yml
    
    * Update tox.ini

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-letsagree",
-    version="1.0.3",
+    version="1.0.4",
     python_requires=">=3.5",
     description=(
         "A django application that associates Groups with Terms "
@@ -41,8 +41,8 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Django",
-        "Framework :: Django :: 2.1",
         "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
