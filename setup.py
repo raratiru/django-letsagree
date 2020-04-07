7843cde7c66b23cab2ee4fae8a14d39806b71f64
George Tantiras <raratiru@users.noreply.github.com>
2020-04-08 00:48:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-letsagree",
-    version="1.1.0",
+    version="1.1.1",
     python_requires=">=3.5",
     description=(
         "A django application that associates Groups with Terms "
