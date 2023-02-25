7ca1e39eedd61639f4ace825249d53a0fec00ee6
George Tantiras <raratiru@users.noreply.github.com>
2023-02-25 15:56:23 +0200

    Python-3.11, Django-4.1 (#101)
    * Python 3.11 - Django 4.1
    
    * Django 4.1 - Python 3.11
    
    * Django 4.1 - Python 3.11
    
    * Django 4.1 - Python 3.11
    
    * Update README.md
    
    * Update setup.py
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,6 @@
 # -*- coding: utf-8 -*-
 
 
-# ==============================================================================
-#
-#       File Name : setup.py
-#
-#       Creation Date : Mon 08 Apr 2019 07:00:40 PM EEST (19:00)
-#
-#       Last Modified : Mon 05 Oct 2020 07:35:24 PM EEST (19:35)
-#
-# ==============================================================================
-
 import os
 from setuptools import find_packages, setup
 
@@ -22,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-letsagree",
-    version="1.1.8",
+    version="1.1.9",
     python_requires=">=3.7",
     description=(
         "A django application that associates Groups with Terms "
@@ -41,9 +31,9 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Django",
-        "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
@@ -53,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Natural Language :: English",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
